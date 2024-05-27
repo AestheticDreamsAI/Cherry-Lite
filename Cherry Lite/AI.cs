a2635c53c63c70a45c8c3c017d4b292fe5f44bc5
AestheticDreamsAi <info@byphilfrei.ch>
2024-05-27 06:38:09 +0200

    AI Class added for handling OpenAi, Ollama or XTTS Api

---
+++ Cherry Lite/AI.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cherry_Lite
+{
+    internal class AI
+    {
+    }
+}
